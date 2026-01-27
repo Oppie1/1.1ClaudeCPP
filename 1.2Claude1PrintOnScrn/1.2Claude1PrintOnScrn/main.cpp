6af16820153860009782c92113605ae96755e193
Oppie1 <atindall729@gmail.com>
2026-01-27 16:06:26 -0600

    Add files via upload

---
+++ 1.2Claude1PrintOnScrn/1.2Claude1PrintOnScrn/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+//Demonstration of the \n (newline) characters.
+//When \n appears in a string, it moves subsequent text to a new line.
+
+int main() {
+
+	cout << " My \n favorite food \n is pizza.";
+
+	return 0;
+}
+
+//Key point: Experiment with spacing and "\n" placement in the sentence
+// e.eg, Delete the space before "My" and lines will no longer align
+// Also, try removing the "\n" chatacters to see how it affects the output
+//
---
+++ 1.3ClaudeVariables/1.3ClaudeVariables/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+//Demonstrates variatible initialization and basic arithmetic operations
+
+int main() {
+
+	//Initialize integer variables
+	int a = 4;
+	int b = 3;
+
+	//Perform arithmetic operations and store results
+	int sum = a + b;//Addition
+	int sum2 = a - b;//Subtraction
+
+	//Display results
+	cout << "a + b = \n";
+	cout << sum << endl;
+	cout << "\na - b = " << endl;
+	cout << sum2 << endl;
+
+	return 0;
+
+}
